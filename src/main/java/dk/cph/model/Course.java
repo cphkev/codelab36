4d6acccb1e173e7ff140ebb3d49f141867420c8e
ScriptCodes <143606581+ScriptCodes@users.noreply.github.com>
2024-09-04 10:00:27 +0200

    Student & Course Push

--- src/main/java/dk/cph/model/Course.java
+++ src/main/java/dk/cph/model/Course.java
@@ -17,7 +17,7 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(name = "description", nullable = false)
+    @Column(name = "course_name", nullable = false)
     private String description;
 
     @Column(name = "start_date", nullable = false)
